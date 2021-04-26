ea254ebd7bbd65df076ecf7fcf512f54cdaf0e01
Danita Forbes <danitacodes@gmail.com>
2021-04-26 14:23:06 -0700

    Completed code for dice game

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const randomNumber1 = Math.floor(Math.random() * (7 - 1) + 1);
+let randomNumber1 = Math.floor(Math.random() * (7 - 1) + 1);
+let randomNumber2 = Math.floor(Math.random() * (7 - 1) + 1);
 
-document.querySelector('.die .img1').setAttribute("src",images1);
+let firstDie = '/Images/die'+ randomNumber1 + '.png';
+let secondDie = '/Images/die'+ randomNumber2 + '.png';
+
+document.querySelector('.img1').setAttribute("src",firstDie);
+document.querySelector('.img2').setAttribute("src",secondDie);
+
+if (randomNumber1 === randomNumber2) {
+    document.querySelector('.container h1').innerHTML='We have a tie';
+} else if (randomNumber1 > randomNumber2){
+    document.querySelector('.container h1').innerHTML='Player 1 Wins';
+}else { (randomNumber1 < randomNumber2)
+    document.querySelector('.container h1').innerHTML='Player 2 Wins';
+}
